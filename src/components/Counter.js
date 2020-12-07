86a0e430a7d9b5d2bd61c05cb7051ce04f048869
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-07 11:03:21 -0600

    quick setup of counter with countReducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
+function countReducer(count, action) {
+  switch (action) {
+    case 'INCREMENT':
+      return count + 1;
+    case 'DECREMENT':
+      return count - 1;
+    case 'RESET':
+      return 0;
+    default:
+      throw new Error(`Action of type ${action.type} is not supported.`);
+  }
+}
+
 const Counter = () => {
+  const [count, dispatch] = React.useReducer(countReducer, 0);
+
   return (
     <main className='counter-container'>
       <section className='display'>
-        <h1>Count: 0</h1>
+        <h1>Count: {count}</h1>
       </section>
       <section className='buttons'>
-        <button>Increment</button>
-        <button>Decrement</button>
-        <button>Reset</button>
+        <button onClick={() => dispatch('INCREMENT')}>Increment</button>
+        <button onClick={() => dispatch('DECREMENT')}>Decrement</button>
+        <button onClick={() => dispatch('RESET')}>Reset</button>
       </section>
     </main>
   );
