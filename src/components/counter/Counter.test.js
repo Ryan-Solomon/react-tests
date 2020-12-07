d887f1604b2e52867543874952870d404c954910
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-07 11:22:11 -0600

    add test for incrementing counter

--- src/components/counter/Counter.test.js
+++ src/components/counter/Counter.test.js
@@ -1,8 +1,15 @@
 import Counter from './Counter';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 it('renders to the page', () => {
   render(<Counter />);
   expect(screen.getByText('Count: 0')).toBeInTheDocument();
+});
+
+it('Clicks on increment and increments the count by 1', () => {
+  render(<Counter />);
+  userEvent.click(screen.getByRole('button', { name: 'Increment' }));
+  expect(screen.getByText('Count: 1')).toBeInTheDocument();
 });
